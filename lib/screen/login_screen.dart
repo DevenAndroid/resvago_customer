7924bce5a0c96db639af431239604d1f0dc8ee0a
EOXYS <deveneoxys@gmail.com>
2023-10-16 10:08:04 +0530

    13-10-23 Aayushi

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -62,195 +62,194 @@ class _LoginScreenState extends State<LoginScreen> {
     var width = MediaQuery.of(context).size.width;
     return Scaffold(
         backgroundColor: Colors.transparent,
-        body: SingleChildScrollView(
-            child: Container(
-                height: Get.height,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.fill,
-                        image: AssetImage(
-                          "assets/images/login.png",
-                        ))),
-                child: SingleChildScrollView(
-                  physics: NeverScrollableScrollPhysics(),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 220,
+        body: Container(
+            height: Get.height,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      "assets/images/login.png",
+                    ))),
+            child: SingleChildScrollView(
+              physics: NeverScrollableScrollPhysics(),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 210,
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'WELCOME BACK',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 28,
                         ),
-                        Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            'WELCOME BACK',
-                            style: GoogleFonts.poppins(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w600,
-                              fontSize: 28,
-                            ),
-                          ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Login your account.',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w300,
+                          fontSize: 14,
+                          // fontFamily: 'poppins',
                         ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            'Login your account.',
-                            style: GoogleFonts.poppins(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w300,
-                              fontSize: 14,
-                              // fontFamily: 'poppins',
-                            ),
-                          ),
-                        ),
+                      ),
+                    ),
 
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 25, vertical: 45),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 45),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Enter Mobile number',
+                            style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 14,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                           CommonTextFieldWidget(
+                            controller: phoneNumberController,
+                            textInputAction: TextInputAction.next,
+                            hint: 'Enter your Mobile number',
+                            keyboardType: TextInputType.number,
+                          ),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                           CommonButton(
+                            onPressed: (){
+                              checkPhoneNumberInFirestore(phoneNumberController.text);                                },
+                            title: 'Login',
+                          ),
+                          const SizedBox(
+                            height: 45,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text(
-                                'Enter Mobile number',
-                                style: GoogleFonts.poppins(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w400,
-                                  fontSize: 14,
-                                ),
+                              Container(
+                                height: 1,
+                                width: 120,
+                                color: Color(0xFFD2D8DC),
                               ),
-                              const SizedBox(
-                                height: 15,
+                              //SizedBox(width: 10,),
+                              Text('Or Login with',
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                  )),
+                              //SizedBox(width: 10,),
+                              Container(
+                                height: 1,
+                                width: 120,
+                                color: Color(0xFFD2D8DC),
                               ),
-                               CommonTextFieldWidget(
-                                controller: phoneNumberController,
-                                textInputAction: TextInputAction.next,
-                                hint: 'Enter your Mobile number',
-                                keyboardType: TextInputType.number,
-                              ),
-                              const SizedBox(
-                                height: 40,
-                              ),
-                               CommonButton(
-                                onPressed: (){
-                                  checkPhoneNumberInFirestore(phoneNumberController.text);                                },
-                                title: 'Login',
-                              ),
-                              const SizedBox(
-                                height: 45,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Container(
-                                    height: 1,
-                                    width: 120,
-                                    color: Color(0xFFD2D8DC),
-                                  ),
-                                  //SizedBox(width: 10,),
-                                  Text('Or Login with',
-                                      style: GoogleFonts.poppins(
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.w500,
-                                        color: Colors.white,
-                                      )),
-                                  //SizedBox(width: 10,),
-                                  Container(
-                                    height: 1,
-                                    width: 120,
-                                    color: Color(0xFFD2D8DC),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(
-                                height: 45,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Container(
-                                    width: 152,
-                                    height: 60,
-                                    decoration: BoxDecoration(
-                                        color: Colors.white.withOpacity(.10),
-                                        borderRadius: BorderRadius.circular(10),
-                                        border: Border.all(color: Colors.white.withOpacity(.35))),
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        Image.asset(
-                                          'assets/icons/facrebook.png',
-                                          height: 27,
-                                        ),
-                                        SizedBox(
-                                          width: 10,
-                                        ),
-                                        Text(
-                                          'Facebook',
-                                          style: GoogleFonts.poppins(
-                                              fontSize: 14, fontWeight: FontWeight.w500, color: Colors.white),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                  GestureDetector(
-                                    onTap: () {},
-                                    child: Container(
-                                      width: 152,
-                                      height: 60,
-                                      decoration: BoxDecoration(
-                                          color: Colors.white.withOpacity(.10),
-                                          borderRadius: BorderRadius.circular(10),
-                                          border: Border.all(color: Colors.white.withOpacity(.35))),
-                                      child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          Image.asset(
-                                            'assets/icons/google.png',
-                                            height: 25,
-                                          ),
-                                          SizedBox(
-                                            width: 10,
-                                          ),
-                                          Text(
-                                            'Google',
-                                            style: GoogleFonts.poppins(
-                                                fontSize: 15, fontWeight: FontWeight.w600, color: Colors.white),
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 50,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    "Don't Have an Account?",
-                                    style:
-                                        GoogleFonts.poppins(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 15),
-                                  ),
-                                  InkWell(
-                                    onTap: () {
-                                      Get.toNamed(MyRouters.signupScreen);
-                                    },
-                                    child: Text(
-                                      '  Signup',
-                                      style: GoogleFonts.poppins(
-                                          color: const Color(0xFFFFBA00), fontWeight: FontWeight.w600, fontSize: 15),
-                                    ),
-                                  )
-                                ],
-                              )
                             ],
                           ),
-                        )
-                      ]),
-                ))));
+                          SizedBox(
+                            height: 45,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                width: 152,
+                                height: 60,
+                                decoration: BoxDecoration(
+                                    color: Colors.white.withOpacity(.10),
+                                    borderRadius: BorderRadius.circular(10),
+                                    border: Border.all(color: Colors.white.withOpacity(.35))),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Image.asset(
+                                      'assets/icons/facrebook.png',
+                                      height: 27,
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      'Facebook',
+                                      style: GoogleFonts.poppins(
+                                          fontSize: 14, fontWeight: FontWeight.w500, color: Colors.white),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {},
+                                child: Container(
+                                  width: 152,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                      color: Colors.white.withOpacity(.10),
+                                      borderRadius: BorderRadius.circular(10),
+                                      border: Border.all(color: Colors.white.withOpacity(.35))),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Image.asset(
+                                        'assets/icons/google.png',
+                                        height: 25,
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text(
+                                        'Google',
+                                        style: GoogleFonts.poppins(
+                                            fontSize: 15, fontWeight: FontWeight.w600, color: Colors.white),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 50,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Don't Have an Account?",
+                                style:
+                                    GoogleFonts.poppins(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 15),
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  Get.toNamed(MyRouters.signupScreen);
+                                },
+                                child: Text(
+                                  '  Signup',
+                                  style: GoogleFonts.poppins(
+                                      color: const Color(0xFFFFBA00), fontWeight: FontWeight.w600, fontSize: 15),
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ]),
+            )));
   }
 }
